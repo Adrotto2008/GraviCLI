a4bf49ae90b986a95c3c64313366432d4b80451d
Adriano_Palmeri <adrianopalmeri02@gmail.com>
2026-04-02 23:28:51 +0200

    refactor skip

--- main.cpp
+++ main.cpp
@@ -51,100 +51,102 @@ int main(void) {
 
     while (!uscita) {
 
-        if (kbhit()){
-            input.scan();
+        if (!kbhit()) {
+            continue;
+        }
 
-            movimento = 1;
+        input.scan();
 
-            TipoInput azione = input.azione();
+        movimento = 1;
 
-            if (azione == TipoInput::INVIO) {
-                switch (input.comando()) {
-                    case TipoComando::DESTRA:
-                        if (gravita.back() != GRAVITA::SINISTRA && gravita.back() != GRAVITA::DESTRA) gravita.push_back(GRAVITA::DESTRA);
-                        break;
+        TipoInput azione = input.azione();
 
-                    case TipoComando::SINISTRA:
-                        if (gravita.back() != GRAVITA::DESTRA && gravita.back() != GRAVITA::SINISTRA) gravita.push_back(
-                            GRAVITA::SINISTRA);
-                        break;
+        if (azione == TipoInput::INVIO) {
+            switch (input.comando()) {
+                case TipoComando::DESTRA:
+                    if (gravita.back() != GRAVITA::SINISTRA && gravita.back() != GRAVITA::DESTRA) gravita.push_back(GRAVITA::DESTRA);
+                    break;
 
-                    case TipoComando::SU:
-                        if (gravita.back() != GRAVITA::GIU && gravita.back() != GRAVITA::SU) gravita.push_back(GRAVITA::SU);
-                        break;
+                case TipoComando::SINISTRA:
+                    if (gravita.back() != GRAVITA::DESTRA && gravita.back() != GRAVITA::SINISTRA) gravita.push_back(GRAVITA::SINISTRA);
+                    break;
 
-                    case TipoComando::GIU:
-                        if (gravita.back() != GRAVITA::SU && gravita.back() != GRAVITA::GIU) gravita.push_back(GRAVITA::GIU);
-                        break;
+                case TipoComando::SU:
+                    if (gravita.back() != GRAVITA::GIU && gravita.back() != GRAVITA::SU) gravita.push_back(GRAVITA::SU);
+                    break;
 
-                    case TipoComando::SKIP:
-                        skip = true;
-                        break;
+                case TipoComando::GIU:
+                    if (gravita.back() != GRAVITA::SU && gravita.back() != GRAVITA::GIU) gravita.push_back(GRAVITA::GIU);
+                    break;
 
-                    default:
-                        break;
-                }
-            } else if (azione == TipoInput::BACKSPACE) {
+                case TipoComando::SKIP:
+                    skip = true;
+                    break;
 
-                if (posizione.size() > 1) {
-
-                    GRAVITA g2 = static_cast<GRAVITA>((static_cast<int>(gravita.back()) + 2) % 4);
-
-                    if (campo.collisione(posizione.back(), g2, movimento) == TipoScritta::LIBERO) {
-                        if (gravita.size() > 1) gravita.pop_back();
-                    }
-
-                    if (!input.stringa.empty()) input.stringa.pop_back();
-
-                    campo.set_casella(posizione.back().Y, posizione.back().X, ' ', TipoScritta::LIBERO);
-                    posizione_cursore(posizione.back());
-                    campo.stampa(posizione.back().Y, posizione.back().X);
-
-                    posizione.pop_back();
-                }
-
-            } else if (azione == TipoInput::DEBUG) {
-                campo.stampa_campo();
-            } else if (azione == TipoInput::ESCI) {
-                uscita = true;
+                default:
+                    break;
             }
+        } else if (azione == TipoInput::BACKSPACE) {
 
-            if (skip) {
-                if (campo.collisione(posizione.back(), gravita.back(), movimento_skip) != TipoScritta::LIBERO) {
-                    //errore
-                    skip = false;
-                    cursore_manuale(50, 1);
-                    printf("aaa");
-                }else {
-                    posizione_skip = spostamento(movimento_skip - 1, gravita.back(), posizione.back());
-                }
-            }
+            if (posizione.size() > 1) {
 
-            if (input.valido()) {
+                GRAVITA g2 = static_cast<GRAVITA>((static_cast<int>(gravita.back()) + 2) % 4);
 
-                if (skip) {//aggiusta
-                    posizione.push_back(spostamento(movimento, gravita.back(), posizione_skip));
-                }else {
-                    if (campo.collisione(posizione.back(), gravita.back(), movimento) != TipoScritta::LIBERO) {
-                        input.stringa.erase(input.stringa.length() - 2, 1);
-                        //rendi rosso il carattere?
-                    }else {
-                        posizione.push_back(spostamento(movimento, gravita.back(), posizione.back()));
-                    }
+                if (campo.collisione(posizione.back(), g2, movimento) == TipoScritta::LIBERO) {
+                    if (gravita.size() > 1) gravita.pop_back();
                 }
 
+                if (!input.stringa.empty()) input.stringa.pop_back();
 
-
-                campo.set_casella(posizione.back().Y, posizione.back().X, input.input, TipoScritta::COLLISIONE);
-
+                campo.set_casella(posizione.back().Y, posizione.back().X, ' ', TipoScritta::LIBERO);
                 posizione_cursore(posizione.back());
-
                 campo.stampa(posizione.back().Y, posizione.back().X);
 
-
-            skip = false;
-
+                posizione.pop_back();
             }
+
+        } else if (azione == TipoInput::DEBUG) {
+            campo.stampa_campo();
+        } else if (azione == TipoInput::ESCI) {
+            uscita = true;
+        }
+
+        if (skip) {
+            if (campo.collisione(posizione.back(), gravita.back(), movimento_skip) != TipoScritta::LIBERO) {
+                //errore
+                skip = false;
+                cursore_manuale(50, 1);
+                printf("aaa");
+            }else {
+                posizione_skip = spostamento(movimento_skip - 1, gravita.back(), posizione.back());
+            }
+        }
+
+        if (input.valido()) {
+
+            if (skip) {//aggiusta
+                posizione.push_back(spostamento(movimento, gravita.back(), posizione_skip));
+            }else {
+                if (campo.collisione(posizione.back(), gravita.back(), movimento) != TipoScritta::LIBERO) {
+                    input.stringa.erase(input.stringa.length() - 2, 1);
+                    //rendi rosso il carattere?
+                }else {
+                    posizione.push_back(spostamento(movimento, gravita.back(), posizione.back()));
+                }
+            }
+
+
+
+            campo.set_casella(posizione.back().Y, posizione.back().X, input.input, TipoScritta::COLLISIONE);
+
+            posizione_cursore(posizione.back());
+
+            campo.stampa(posizione.back().Y, posizione.back().X);
+
+
+        skip = false;
+
+
 
 
         }
